d0c72068e239c90815231b835eaa0ca136db2700
kiattisin-kan <51523556+kiattisin-kan@users.noreply.github.com>
2020-06-06 21:21:55 +0700

    Update bot_userid.php

--- bot_userid.php
+++ bot_userid.php
@@ -1,35 +1,77 @@
-<?php
+<?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
 
-$API_URL = 'https://api.line.me/v2/bot/message';
-//###### Token Line Bot Register ######//
-$accessToken = "dhreZK83Nt7uaCxqJmZkRh8aebR3Qm6hf7aNQI85YaGiFQhJYWSPy/6Mc2jS/dSFh3oMjY8wyST2ysR78fTFIQy1FxcNtEoK+5F7AXV4HSgggwE9S+sr2W7Xm1H9s7u3IhvB+GfYZkpRwVC3PZZPgAdB04t89/1O/w1cDnyilFU=";
- 
-$request = file_get_contents('php://input');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
- 
-$arrayHeader = array();
-$arrayHeader[] = "Content-Type: application/json";
-$arrayHeader[] = "Authorization: Bearer {$accessToken}";
-echo "test";
-//########### Find IDUser IDGroup IDRoom From Source ###############//
-if ( sizeof($request_array['events']) > 0 ) {
-    foreach ($request_array['events'] as $event) {
-        $reply_message = '';
-        $reply_token = $event['replyToken'];
- 
-    if(isset($event['source']['userId'])){
-        $id = $event['source']['userId'];
-    } else if(isset($event['source']['groupId'])){
-        $id = $event['source']['groupId'];
-    } else if(isset($event['source']['room'])){
-        $id = $event['source']['room'];
-    }
- 
-    $text = $event['message']['text'];
-                $arrayPostData['replyToken'] = $request_array['events'][0]['replyToken'];
-                $arrayPostData['messages'][0]['type'] = "text";
-                $arrayPostData['messages'][0]['text'] = $id;
-                replyMsg($arrayHeader,$arrayPostData);
-    }//Close For.
-}//Close If.
+	//file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+	$replyToken = $deCode['events'][0]['replyToken'];
+	$userId = $deCode['events'][0]['source']['userId'];
+	$text = $deCode['events'][0]['message']['text'];
+
+	$messages = [];
+	$messages['replyToken'] = $replyToken;
+ $temp = $userId . "ทดสอบแจ้งเตือน";
+	$messages['messages'][0] = getFormatTextMessage($temp);
+
+	$encodeJson = json_encode($messages);
+
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+ $LINEDatas['token'] = "dhreZK83Nt7uaCxqJmZkRh8aebR3Qm6hf7aNQI85YaGiFQhJYWSPy/6Mc2jS/dSFh3oMjY8wyST2ysR78fTFIQy1FxcNtEoK+5F7AXV4HSgggwE9S+sr2W7Xm1H9s7u3IhvB+GfYZkpRwVC3PZZPgAdB04t89/1O/w1cDnyilFU=";
+
+ $results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
+	http_response_code(200);
+
+	function getFormatTextMessage($text)
+	{
+		$datas = [];
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
 ?>
