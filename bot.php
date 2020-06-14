b97a793340fc207c8bbf53fdf507eecb0bb804aa
kiattisin-kan <51523556+kiattisin-kan@users.noreply.github.com>
2020-06-14 13:59:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -66,7 +66,7 @@ if (!is_null($events['events'])) {
 			getMqttfromlineMsg($Topic,$text);
 	//		$results = sentMessage($encodeJson,$LINEDatas);
 	//	}
-	}
+	//}
 
 	/*Return HTTP Request 200*/
 	http_response_code(200);
