61f9f2403b8379b21c882335367902bdec831780
kiattisin-kan <51523556+kiattisin-kan@users.noreply.github.com>
2020-06-14 11:17:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -53,6 +53,14 @@ if (!is_null($events['events'])) {
  $LINEDatas['token'] = "dhreZK83Nt7uaCxqJmZkRh8aebR3Qm6hf7aNQI85YaGiFQhJYWSPy/6Mc2jS/dSFh3oMjY8wyST2ysR78fTFIQy1FxcNtEoK+5F7AXV4HSgggwE9S+sr2W7Xm1H9s7u3IhvB+GfYZkpRwVC3PZZPgAdB04t89/1O/w1cDnyilFU=";
 
  $results = sentMessage($encodeJson,$LINEDatas);
+	
+	
+	if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+	{
+		$Topic = "NamphongPP" ;
+		getMqttfromlineMsg($Topic,$text);
+	}
+	
 
 	/*Return HTTP Request 200*/
 	http_response_code(200);
